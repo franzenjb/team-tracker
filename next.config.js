7d4fe21aa7a8096098c7121c5cf0bc770ec5bf7f
Jeff Franzen <jefffranzen@Jeffs-MacBook-Air.local>
2025-10-14 21:28:12 -0400

    Allow iframe embedding in Experience Builder

--- next.config.js
+++ next.config.js
@@ -5,6 +5,23 @@ const nextConfig = {
       bodySizeLimit: '2mb',
     },
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://*.arcgis.com https://*.esri.com",
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
